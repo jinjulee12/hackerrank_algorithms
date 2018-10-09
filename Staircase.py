45b5b6c4d0195bd9fdfb17ea494523e97bf0da6b
Jinju Lee <42167113+jinjulee12@users.noreply.github.com>
2018-10-09 23:40:58 +0900

    Update Staircase.py

--- Staircase.py
+++ Staircase.py
@@ -4,11 +4,10 @@ Domain    : Python
 Created   : 08 Oct 2018
 '''
 
-#Wrong!!!!
-
 n = int(input())
 a = 1
+
 while n > 0 :
-    print(' '*(n-a), '#'*a)
+    print(' '*(n-1),'#'*a)
     n -= 1
     a += 1
